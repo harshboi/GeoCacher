e800c0fb7eb1c847a8ab2986a6c6acd33581ee5b
Trevor-Jones <trevor.eric.j@gmail.com>
2018-06-11 10:59:25 -0700

    added initial server file

---
+++ server.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res, next) {
+  res.render('mapList', {
+    twits: twitData,
+    showAddBtn: true
+  });
+});
+
+app.get('/:n', function (req, res, next) {\
+  // Display location n
+});
+
+app.get('*', function (req, res, next) {
+  res.status(404);
+  res.render('404page');
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
