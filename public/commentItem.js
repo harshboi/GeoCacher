5b4017f2403125030243ce61e5a9c0604038eca2
Harshvardhan Singh <singhhar@oregonstate.edu>
2018-06-12 21:42:43 -0700

    Fixed merge errors for db operations

--- public/commentItem.js
+++ public/commentItem.js
@@ -3,10 +3,10 @@
 templates['commentItem'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<article class=\"comment\">\n  <div class=\"comment-content\">\n    <p class=\"comment-author\">"
+  return "<article class=\"comment\">\r\n  <div class=\"comment-content\">\r\n    <p class=\"comment-author\">"
     + alias4(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data}) : helper)))
-    + "</p>\n    <p class=\"comment-text\">"
+    + "</p>\r\n    <p class=\"comment-text\">"
     + alias4(((helper = (helper = helpers.comment || (depth0 != null ? depth0.comment : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"comment","hash":{},"data":data}) : helper)))
-    + "</p>\n  </div>\n</article>\n";
+    + "</p>\r\n  </div>\r\n</article>\r\n";
 },"useData":true});
 })();
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var fs = require('fs');
 
 var path = require('path');
@@ -17,17 +16,30 @@ var mongoUser = process.env.MONGO_USER;
 var mongoPassword = process.env.MONGO_PASSWORD;
 var mongoDBName = process.env.MONGO_DB_NAME;
 mongoUser = 'cs290_singhhar';
-mongoHost = 'cs290_singhhar';
+mongoHost = 'classmongo.engr.oregonstate.edu';
 mongoDBName = 'cs290_singhhar';
 mongoPassword = 'cs290_singhhar';
-var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
-// var mongoURL = 'mongodb://' + 'cs290_singhhar' + ':' + 'cs290_singhhar' + '@' + 'classmongo.engr.oregonstate.edu' + ':' + mongoPort + '/' + 'cs290_singhhar';
+// var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+var mongoURL = 'mongodb://' + 'cs290_singhhar' + ':' + 'cs290_singhhar' + '@' + 'classmongo.engr.oregonstate.edu' + ':' + mongoPort + '/' + 'cs290_singhhar';
+
 // mongo --host classmongo.engr.oregonstate.edu --username cs290_singhhar cs290_singhhar --password
 //mongo --host classmongo.engr.oregonstate.edu --username cs290_[ONID] cs290_[ONID] --password
+// mongodb://cs290_singhhar:cs290_singhhar@classmongo.engr.oregonstate.edu:27017/cs290_singhhar
+var mongoDBDatabase;
+var db;
+
 MongoClient.connect(mongoURL, function(err, client) {
   if (err) {
-    //throw err;
+    throw err;
   }
+  else {
+  }
+  db = mongoDBDatabase = client.db(mongoDBName);
+  // console.log("db is: ", db)
+  app.listen(3001, function () {
+    console.log("== Server listening on port 3000");
+  });
+  retrieve_all_data();
 });
 
 function add_data (_name, _link, _author, _city, _state, _lat, _long) {
@@ -106,6 +118,8 @@ app.get('/location/:n', function (req, res, next) {
     next();
   }
 });
+
+
 
 
 app.get('/', function (req, res, next) {
