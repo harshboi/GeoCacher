e2ba61257382618f00157efac43fa1a32dad2152
Trevor-Jones <trevor.eric.j@gmail.com>
2018-06-12 20:28:04 -0700

    added modal to handlebars

--- public/index.js
+++ public/index.js
@@ -1,22 +1,37 @@
 function showModal()
 {
 	document.getElementById('modal-backdrop').classList.toggle('hidden');
-	document.getElementById('leave-comment-modal').classList.toggle('hidden');
-	document.getElementById('create-location-modal').classList.toggle('hidden');
+	if(document.getElementById('leave-comment-modal')) {
+		document.getElementById('leave-comment-modal').classList.toggle('hidden');
+	}
+	if(document.getElementById('create-location-modal')) {
+		document.getElementById('create-location-modal').classList.toggle('hidden');
+	}
 }
 
 function hideModal()
 {
 	document.getElementById('modal-backdrop').classList.toggle('hidden');
-	document.getElementById('leave-comment-modal').classList.toggle('hidden');
-	document.getElementById('create-location-modal').classList.toggle('hidden');
-	document.getElementById('comment-author-input').value = '';
-	document.getElementById('location-author-input').value = '';
-	document.getElementById('location-description-input').value = '';
-	document.getElementById('location-latitude-input').value = '';
-	document.getElementById('location-longitude-input').value = '';
-	document.getElementById('location-city-input').value = '';
-	document.getElementById('location-state-input').value = '';
+	if(document.getElementById('leave-comment-modal')) {
+		document.getElementById('leave-comment-modal').classList.toggle('hidden');
+	}
+	if(document.getElementById('create-location-modal')) {
+		document.getElementById('create-location-modal').classList.toggle('hidden');
+	}
+	if(document.getElementById('comment-author-input'))
+		document.getElementById('comment-author-input').value = '';
+	if(document.getElementById('location-author-input'))
+		document.getElementById('location-author-input').value = '';
+	if(document.getElementById('location-description-input'))
+		document.getElementById('location-description-input').value = '';
+	if(document.getElementById('location-latitude-input'))
+		document.getElementById('location-latitude-input').value = '';
+	if(document.getElementById('location-longitude-input'))
+		document.getElementById('location-longitude-input').value = '';
+	if(document.getElementById('location-city-input'))
+		document.getElementById('location-city-input').value = '';
+	if(document.getElementById('location-state-input'))
+		document.getElementById('location-state-input').value = '';
 }
 
 document.getElementsByClassName('modal-close-button')[0].addEventListener('click', hideModal);
--- public/locationListItem.js
+++ public/locationListItem.js
@@ -3,20 +3,20 @@
 templates['locationListItem'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<tr>\r\n  <td class = \"location-name\"><a href="
+  return "<tr>\n  <td class = \"location-name\"><a href="
     + alias4(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"link","hash":{},"data":data}) : helper)))
     + ">"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</a></td>\r\n  <td>"
+    + "</a></td>\n  <td>"
     + alias4(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data}) : helper)))
-    + "</td>\r\n  <td class = \"location-aproximate\">"
+    + "</td>\n  <td class = \"location-aproximate\">"
     + alias4(((helper = (helper = helpers.city || (depth0 != null ? depth0.city : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"city","hash":{},"data":data}) : helper)))
-    + "</td>\r\n  <td class = \"location-aproximate\">"
+    + "</td>\n  <td class = \"location-aproximate\">"
     + alias4(((helper = (helper = helpers.state || (depth0 != null ? depth0.state : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"state","hash":{},"data":data}) : helper)))
-    + "</td>\r\n  <td>"
+    + "</td>\n  <td>"
     + alias4(((helper = (helper = helpers.lat || (depth0 != null ? depth0.lat : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"lat","hash":{},"data":data}) : helper)))
-    + "</td>\r\n  <td>"
+    + "</td>\n  <td>"
     + alias4(((helper = (helper = helpers["long"] || (depth0 != null ? depth0["long"] : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"long","hash":{},"data":data}) : helper)))
-    + "</td>\r\n</tr>\r\n";
+    + "</td>\n</tr>\n";
 },"useData":true});
 })();
